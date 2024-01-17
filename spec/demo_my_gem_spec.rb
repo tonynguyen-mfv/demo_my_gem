9d0771e1d372e5e92ae9ad09117e733c1b7425a5
Tony <nguyen.truong.sinh@moneyforward.vn>
2024-01-17 22:22:19 +0700

    first commit

---
+++ spec/demo_my_gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DemoMyGem do
+  it "has a version number" do
+    expect(DemoMyGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
